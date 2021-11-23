b11f2e72abb722f1bd340e7893e8ffbf24d7a917
jack-van-holland <60764738+jack-van-holland@users.noreply.github.com>
2021-11-23 08:53:30 -0700

    implement model module

--- sklearn_example.ml
+++ sklearn_example.ml
@@ -1,10 +1,58 @@
 module Np = Np.Numpy
+
+open Sklearn.Svm
+open Core
+
+type song = {name: string; id: string; features: Np.Ndarray.t;}
+
+type playlist = {name: string; id: string; features: Np.Ndarray.t;}
+type svm = {hyperplane: int; class1: string; class2: string}
+
+module Model = struct 
+  (* the model *)
+  type t = {hyperplane: Np.Ndarray.t; class1: string; class2: string}
+
+  (* save a model into a file with a give filename *)
+  let save (svc: t) (file: string) : unit = 
+    match svc with |
+      {hyperplane; class1; class2} 
+      -> Stdio.Out_channel.create file |> fun (f) 
+         -> Stdio.Out_channel.output_string f (class1 ^ "\n" ^ class2 ^ "\n" ^ (Np.Ndarray.to_string hyperplane))
+
+  (* open up a model file with a given filename, parse a model object from it *)
+  let load (file: string) : t =
+    match Stdio.In_channel.read_lines file with 
+    | class1 :: class2 :: arr :: [] -> String.split ~on:' ' arr 
+                                       |> List.map ~f:Float.of_string |> Np.Ndarray.of_float_list 
+                                       |> fun (hyperplane) -> {hyperplane; class1; class2;}
+    | _-> failwith "Improper file formatting"
+
+  (* train a binary classifier on two playlists represented as tensors *)
+  let train (p1: playlist) (p2: playlist) : t =
+    let x, y = match p1, p2 with
+      | {features = features1; _}, {features = features2; _} 
+        -> Array.init (Np.size features1) ~f:(fun _ -> -1) |> fun (arr1) 
+           -> (Np.append ~arr:features1 () ~values:features2), 
+              Array.append arr1 @@ Array.init (Np.size features2) ~f:(fun _ -> 1) |> Np.Ndarray.of_int_array 
+    in let clf = LinearSVC.create ~c:1.0 () 
+    in match p1, p2 with 
+    | {name = class1; _}, {name = class2; _} 
+      -> {hyperplane = (LinearSVC.fit clf ~x ~y |> fun (svm) 
+                        -> Np.append ~arr:(LinearSVC.coef_ svm) () ~values:(LinearSVC.intercept_ svm)); class1; class2}
+
+  (* classify a song represented by a vector into one of the two playlists true = first, false = second *)
+  let classify (svc: t) (s: song) : string =
+    match svc, s with 
+    | {hyperplane; class1; class2}, {features; _} -> 
+      if 0 < Array.get (Np.Ndarray.to_int_array @@ Np.dot ~b:hyperplane features) 0 then class1 else class2
+
+end 
+
 let () = 
-  let n_samples, n_features = 10, 5 in
-  Np.Random.seed 0;
-  let y = Np.Random.uniform ~size:[n_samples] () in
-  let x = Np.Random.uniform ~size:[n_samples; n_features] () in
-  let open Sklearn.Svm in
-  let clf = SVR.create ~c:1.0 ~epsilon:0.2 () in
-  Format.printf "%a\n" SVR.pp @@ SVR.fit clf ~x ~y;
-  Format.printf "%a\n" Np.pp @@ SVR.support_vectors_ clf;
+  let y = Np.vectori [| 1; 1; 2; 2 |] in
+  let x = Np.matrixf [| [| -1.; -1. |]; [| -2.; -1. |]; [| -1.; -1. |]; [| 2.; 1. |] |]
+  in
+  let clf = LinearSVC.create ~c:1.0 () in
+  Format.printf "%a\n" LinearSVC.pp @@ LinearSVC.fit clf ~x ~y;
+  Format.printf "%a\n" Np.pp (LinearSVC.coef_ clf);
+  Format.printf "%a\n" Np.pp (LinearSVC.intercept_ clf);
