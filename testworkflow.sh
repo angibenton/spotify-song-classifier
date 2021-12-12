90c40d06f7abbd5874024f92b064ac3664597211
angibenton <angi.benton@jhu.edu>
2021-12-11 19:51:17 -0500

    bash script that can be helpful for using the executable

---
+++ testworkflow.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CLASSICALPLAYLISTID=1h0CEZCm6IbFTbxThn6Xcs
+TRAPPLAYLISTID=68PbMiU12ssvm5UAHsqqOi
+MOONLIGHTSONGID=1LNqr2FALBNFX1smNJcKD2
+
+
+DATASETFOLDER=trap_vs_classical
+MODELFILE=trap_vs_classical_model.txt
+
+source .venv/bin/activate
+dune build 
+dune exec -- ./src/run.exe download --pos-id $TRAPPLAYLISTID --neg-id $CLASSICALPLAYLISTID --dataset-folder $DATASETFOLDER --test 0.2 --val 0.2
+dune exec -- ./src/run.exe train --dataset-folder $DATASETFOLDER --model-file $MODELFILE
+dune exec -- ./src/run.exe test --dataset-folder $DATASETFOLDER --model-file $MODELFILE
+dune exec -- ./src/run.exe classify --model-file $MODELFILE --song-id $MOONLIGHTSONGID
+deactivate 
+
